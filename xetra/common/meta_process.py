60f8c443c0d7cb98efa1565d8320f6d87076f1ed
Geenee <34036481+geeneehime@users.noreply.github.com>
2022-01-16 23:03:59 +0400

    added list_files_in_prefix and unittests

---
+++ xetra/common/meta_process.py
@@ -0,0 +1,18 @@
+"""
+Methods for processing with the meta file
+"""
+
+class MetaProcess():
+    """
+    class for working with the meta file
+    """
+
+    @staticmethod
+    def update_meta_file():
+        pass
+
+    @staticmethod
+    def return_date_list():
+        pass
+
+    
